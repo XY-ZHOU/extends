2f4c3e720106ecca7af94aee9ef243529e14b9be
XY-ZHOU <731986251@qq.com>
2019-02-12 22:11:03 +0800

    add class of cat

---
+++ Extends/src/Main/Cat.java
@@ -0,0 +1,13 @@
+package Main;
+
+public class Cat extends Animal {
+    public Cat(int age,int weight)
+    {
+        super(age,weight);
+    }
+
+    public void action()
+    {
+        System.out.println("喵喵叫");
+    }
+}
