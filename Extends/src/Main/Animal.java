816faeea1f177d9f7f003467b64bfef8addc3972
XY-ZHOU <731986251@qq.com>
2019-02-12 22:10:41 +0800

    add class of animal

--- Extends/src/Main/Animal.java
+++ Extends/src/Main/Animal.java
@@ -1,4 +1,35 @@
 package Main;
 
 public class Animal {
+    protected int age;
+    protected int weight;
+
+    public Animal(int age, int weight) {
+        this.age = age;
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void run() {
+        System.out.println("跑跳");
+    }
+
+    public void eat() {
+        System.out.println("吃东西");
+    }
 }
